1fc45ae0e70221f3c1dba8e7b8a70db97a91a2eb
Daniel Soto <dasg.1993@gmail.com>
2023-04-20 14:33:02 -0500

    [feature] - setup unit test with testing library and cover 100% and clean app ts file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:testing-library/react',
+    'plugin:jest-formatting/strict',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {sourceType: 'module'},
+  plugins: ['@typescript-eslint', 'testing-library', 'jest-formatting'],
+  ignorePatterns: [
+    'node_modules/',
+    'coverage/',
+    'android/',
+    'ios/',
+    'vendor/',
+    '.jest/',
+  ],
 };
